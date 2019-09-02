a9480d24e39f48a57f803fd17c090a1dfbf4ec0b
Cheri-Cheri <cht.m249@gmail.com>
2019-09-02 20:57:47 +0900

    花火とボタン

---
+++ Assets/touchtouch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchtouch : MonoBehaviour
+{
+    public AudioClip[] aClips;
+    public AudioSource myAudioSource;
+    string btnName;
+    // Start is called before the first frame update
+    void Start()
+    {
+         myAudioSource = GetComponent<AudioSource>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);            
+            RaycastHit Hit;
+            if (Physics.Raycast(ray, out Hit)) {
+               //Debug.Log(Hit.collider.gameObject.transform.position);　//タッチしたマウスの座標表示
+               btnName = Hit.transform.name;
+               switch (btnName)
+               {
+                   case "Cube1":
+                    myAudioSource.clip = aClips[0];
+                    myAudioSource.Play();
+                    Debug.Log("Button 1 Pressed");
+                    break;
+                   case "Cube2":
+                    myAudioSource.clip = aClips[1];
+                    myAudioSource.Play();
+                    Debug.Log("Button 2 Pressed");
+                    break;
+                   default:
+                    Debug.Log("NOT Pressed");
+                    break;
+               }
+            }
+        }
+        
+    }
+}
